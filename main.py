3dee821f5d1b03331d3cc47e43f285c78e4ee848
thsipsis <th.sipsis@ufl.edu>
2024-10-21 00:14:05 -0400

    Initial commit of main and encoder

---
+++ main.py
@@ -0,0 +1,37 @@
+#Main function and encode function written by Theofanis Sipsis
+
+#Our password
+pw = ""
+
+#We assume that these are valid numbers, no error/exception checking
+def encode(s):
+    #reset our password
+    global pw
+    pw = ""
+    for c in s:
+        c = (int(c) + 3) % 10
+        pw = f"{pw}{c}"
+
+def main():
+    #Our eternal loop
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        op = input("Please enter an option: ")
+        if op == "1":
+            encode(input("Please enter your password to encode: "))
+        elif op == "2":
+            print("NOT IMPLEMENTED")
+        #We'll just quit on any invalid input or 3, assignment doesn't mention
+        #we need to handle invalid input
+        else:
+            break
+        print()
+
+if __name__ == "__main__":
+    main()
